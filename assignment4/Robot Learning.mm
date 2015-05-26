1859654d24d4f93a67cc01a63e39c37643fdef09
Bastian Lang <bastian.lang@smail.inf.h-brs.de>
2015-05-26 15:23:42 +0200

    initialized repository

---
+++ assignment4/Robot Learning.mm
@@ -0,0 +1,95 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1430237330613" ID="ID_1649218939" MODIFIED="1430285508441" TEXT="Robot Learning">
+<node CREATED="1430237821913" ID="ID_243156320" MODIFIED="1430237825180" POSITION="right" TEXT="adaption"/>
+<node CREATED="1430237834131" ID="ID_683966571" MODIFIED="1430237841170" POSITION="right" TEXT="artificial intelligence"/>
+<node CREATED="1430237841811" ID="ID_1724915279" MODIFIED="1430237852815" POSITION="right" TEXT="autonomous learning robtos"/>
+<node CREATED="1430237880428" ID="ID_1434582299" MODIFIED="1430237887570" POSITION="right" TEXT="cognitive systems">
+<node CREATED="1430284874535" ID="ID_1371109874" MODIFIED="1430284876003" TEXT="Kirsch, A. (2009). Robot learning language - Integrating programming and learning for cognitive systems. Robotics and Autonomous Systems, 57(9), 943&#x2013;954. http://doi.org/10.1016/j.robot.2009.05.001"/>
+</node>
+<node CREATED="1430237915850" ID="ID_567162133" MODIFIED="1430237923141" POSITION="right" TEXT="exploration">
+<node CREATED="1430284939639" ID="ID_802260384" MODIFIED="1430284971855" TEXT="Tani, J., &amp; Yamamoto, J. (2002). On the dynamics of robot exploration learning. Cognitive Systems Research, 3(3), 459&#x2013;470. http://doi.org/10.1016/S1389-0417(02)00052-9"/>
+</node>
+<node CREATED="1430238080947" ID="ID_1918650736" MODIFIED="1430238089389" POSITION="right" TEXT="machine learning"/>
+<node CREATED="1430238699355" ID="ID_1076472257" MODIFIED="1430238803852" POSITION="left" TEXT="Learning Methods">
+<node CREATED="1430238569570" ID="ID_635287318" MODIFIED="1430238573680" TEXT="unsupervised learning">
+<node CREATED="1430238231434" ID="ID_973557110" MODIFIED="1430238265935" TEXT="social learning"/>
+</node>
+<node CREATED="1430238564856" ID="ID_1510031686" MODIFIED="1430238569329" TEXT="supervised learning">
+<node CREATED="1430238202416" ID="ID_1203710111" MODIFIED="1430238230782" TEXT="reinforcement learning">
+<node CREATED="1430237825474" ID="ID_871201089" MODIFIED="1430914716807" TEXT="apprenticeship learning">
+<node CREATED="1430284761318" ID="ID_1967669721" MODIFIED="1430284762902" TEXT="Abbeel, P., &amp; Ng, A. Y. (2004). Apprenticeship learning via inverse reinforcement learning. In Proceedings of the 21st International Conference on Machine Learning (ICML) (pp. 1&#x2013;8). http://doi.org/10.1145/1015330.1015430"/>
+</node>
+<node CREATED="1430237923514" ID="ID_791634734" MODIFIED="1430914715510" TEXT="imitation learning">
+<node CREATED="1430285030223" ID="ID_422713115" MODIFIED="1430285032091" TEXT="Kroemer, O. B., Detry, R., Piater, J., &amp; Peters, J. (2010). Combining active learning and reactive control for robot grasping. Robotics and Autonomous Systems, 58(9), 1105&#x2013;1116. http://doi.org/10.1016/j.robot.2010.06.001"/>
+<node CREATED="1430285033040" ID="ID_1804640050" MODIFIED="1430285041706" TEXT="Gu, Y., Thobbi, A., &amp; Sheng, W. (2011). Human-robot collaborative manipulation through imitation and reinforcement learning. In 2011 IEEE International Conference on Information and Automation, ICIA 2011 (pp. 151&#x2013;156). http://doi.org/10.1109/ICINFA.2011.5948979"/>
+</node>
+<node CREATED="1430238121361" ID="ID_186064702" MODIFIED="1430914718694" TEXT="modular q-learning">
+<node CREATED="1430285580995" ID="ID_1954939287" MODIFIED="1430285582102" TEXT="Park, K. H., Kim, Y. J., &amp; Kim, J. H. (2001). Modular Q-learning based multi-agent cooperation for robot soccer. Robotics and Autonomous Systems, 35(2), 109&#x2013;122. http://doi.org/10.1016/S0921-8890(01)00114-2"/>
+</node>
+<node CREATED="1430237887964" ID="ID_435950841" MODIFIED="1430914719172" TEXT="em algorithm">
+<node CREATED="1430284924803" ID="ID_737693139" MODIFIED="1430284926158" TEXT="Vlassis, N., Toussaint, M., Kontes, G., &amp; Piperidis, S. (2009). Learning Model-free robot control by a Monte Carlo em algorithm. Autonomous Robots, 27(2), 123&#x2013;130. http://doi.org/10.1007/s10514-009-9132-0"/>
+</node>
+</node>
+</node>
+<node CREATED="1430238317850" ID="ID_87279336" MODIFIED="1430238323394" TEXT="transfer learning"/>
+<node CREATED="1430238293285" ID="ID_170357402" MODIFIED="1430238317318" TEXT="trajectory learning"/>
+<node CREATED="1430238267582" ID="ID_935142762" MODIFIED="1430238292474" TEXT="sparse incremental learning"/>
+<node CREATED="1430238170333" ID="ID_852747776" MODIFIED="1430914694843" TEXT="q-learning">
+<node CREATED="1430285716461" ID="ID_1071195420" MODIFIED="1430285719022" TEXT="Wang, Y., &amp; de Silva, C. W. (2008). A machine-learning approach to multi-robot coordination. Engineering Applications of Artificial Intelligence, 21(3), 470&#x2013;484. http://doi.org/10.1016/j.engappai.2007.05.006"/>
+</node>
+<node CREATED="1430238160824" ID="ID_414610407" MODIFIED="1430914695845" TEXT="one-shot learning">
+<node CREATED="1430285663107" ID="ID_283038510" MODIFIED="1430285664068" TEXT="Dillmann, R. (2004). Teaching and learning of robot tasks via observation of human performance. In Robotics and Autonomous Systems (Vol. 47, pp. 109&#x2013;116). http://doi.org/10.1016/j.robot.2004.03.005"/>
+</node>
+<node CREATED="1430238089904" ID="ID_396409036" MODIFIED="1430914696252" TEXT="model learning">
+<node CREATED="1430285566801" ID="ID_1445274814" MODIFIED="1430285568205" TEXT="Nguyen-Tuong, D., &amp; Peters, J. (2011). Model learning for robot control: A survey. Cognitive Processing. http://doi.org/10.1007/s10339-011-0404-1"/>
+</node>
+<node CREATED="1430237997096" ID="ID_593937909" MODIFIED="1430914696604" TEXT="learning by imitation">
+<node CREATED="1430285173796" ID="ID_1957793810" MODIFIED="1430285175907" TEXT="Cakmak, M., Depalma, N., Arriaga, R. I., &amp; Thomaz, A. L. (2010). Exploiting social partners in robot learning. Autonomous Robots, 29(3-4), 309&#x2013;329. http://doi.org/10.1007/s10514-010-9197-9"/>
+<node CREATED="1430285176287" ID="ID_797594105" MODIFIED="1430285183740" TEXT="Calinon, S., Guenter, F., &amp; Billard, A. (2007). On learning, representing, and generalizing a task in a humanoid robot. IEEE Transactions on Systems, Man, and Cybernetics, Part B: Cybernetics, 37(2), 286&#x2013;298. http://doi.org/10.1109/TSMCB.2006.886952"/>
+</node>
+<node CREATED="1430237973109" ID="ID_280071106" MODIFIED="1430914697164" TEXT="incremental learning">
+<node CREATED="1430285061080" ID="ID_507330317" MODIFIED="1430285062198" TEXT="Schaal, S., Atkeson, C. G., &amp; Vijayakumar, S. (2002). Scalable techniques from nonparametric statistics for real time robot learning. Applied Intelligence, 17(1), 49&#x2013;60. http://doi.org/10.1023/A:1015727715131"/>
+</node>
+<node CREATED="1430238039374" ID="ID_1622304192" MODIFIED="1430914697854" TEXT="learning by demonstration">
+<node CREATED="1430285153645" ID="ID_551003228" MODIFIED="1430285154993" TEXT="Nicolescu, M. N., &amp; Mataric, M. J. (2003). Natural methods for robot task learning. In Proceedings of the Second International Joint Conference on Autonomous Agents and Multiagent systems - AAMAS &#x2019;03 (p. 241). http://doi.org/10.1145/860575.860614"/>
+<node CREATED="1430285244505" ID="ID_123869772" MODIFIED="1430285247803" TEXT="Argall, B. D., Argall, B. D., Chernova, S., Chernova, S., Veloso, M., Veloso, M., &#x2026; Browning, B. (2009). A survey of robot learning from demonstration. Robotics and Autonomous Systems, 57(5), 469&#x2013;483. http://doi.org/10.1016/j.robot.2008.10.024"/>
+</node>
+<node CREATED="1430237873570" ID="ID_558850827" MODIFIED="1430914699043" TEXT="coevolution">
+<node CREATED="1430284856246" ID="ID_1147380890" MODIFIED="1430284857756" TEXT="Barfoot, T. D., Earon, E. J. P., &amp; D&#x2019;Eleuterio, G. M. T. (2006). Experiments in learning distributed control for a hexapod robot. Robotics and Autonomous Systems, 54(10), 864&#x2013;872. http://doi.org/10.1016/j.robot.2006.04.009"/>
+</node>
+<node CREATED="1430237812335" ID="ID_1060255584" MODIFIED="1430914699516" TEXT="active learning">
+<node CREATED="1430284510934" ID="ID_1765997120" MODIFIED="1430284515017" TEXT="Cakmak, M., &amp; Thomaz, A. L. (2012). Designing robot learners that ask good questions. In Proceedings of the seventh annual ACM/IEEE international conference on Human-Robot Interaction - HRI &#x2019;12 (p. 17). http://doi.org/10.1145/2157689.2157693"/>
+</node>
+<node CREATED="1430237853442" ID="ID_1016941510" MODIFIED="1430914700634" TEXT="batch reinforcement learning">
+<node CREATED="1430284626218" HGAP="26" ID="ID_1262476070" MODIFIED="1430284783685" TEXT="Riedmiller, M., Gabel, T., Hafner, R., &amp; Lange, S. (2009). Reinforcement learning for robot soccer. Autonomous Robots, 27(1), 55&#x2013;73. http://doi.org/10.1007/s10514-009-9120-4"/>
+</node>
+<node CREATED="1430237862464" ID="ID_1156097568" MODIFIED="1430914701120" TEXT="biologically inspired learning">
+<node CREATED="1430284805431" ID="ID_1777390227" MODIFIED="1430284806711" TEXT="Cakmak, M., Depalma, N., Arriaga, R. I., &amp; Thomaz, A. L. (2010). Exploiting social partners in robot learning. Autonomous Robots, 29(3-4), 309&#x2013;329. http://doi.org/10.1007/s10514-010-9197-9"/>
+</node>
+</node>
+<node CREATED="1430238150642" ID="ID_451650071" MODIFIED="1430238160603" POSITION="right" TEXT="object recognition">
+<node CREATED="1430285636555" ID="ID_696821949" MODIFIED="1430285638333" TEXT="Gopalakrishnan, A., Greene, S., &amp; Sekmen, A. (2005). Vision-based mobile robot learning and navigation. In Proceedings - IEEE International Workshop on Robot and Human Interactive Communication (Vol. 2005, pp. 48&#x2013;53). http://doi.org/10.1109/ROMAN.2005.1513755"/>
+</node>
+<node CREATED="1430238130407" ID="ID_523873188" MODIFIED="1430914702628" POSITION="left" TEXT="neural networks">
+<node CREATED="1430285614668" ID="ID_799287813" MODIFIED="1430285616800" TEXT="Wermter, S., Weber, C., Elshaw, M., Panchev, C., Erwin, H., &amp; Pulverm&#x308;uller, F. (2004). Towards multimodal neural robot learning. In Robotics and Autonomous Systems (Vol. 47, pp. 171&#x2013;175). http://doi.org/10.1016/j.robot.2004.03.011"/>
+</node>
+<node CREATED="1430284008066" ID="ID_477970502" MODIFIED="1430284037657" POSITION="left" TEXT="robot curiosity"/>
+<node CREATED="1430284040948" ID="ID_831978169" MODIFIED="1430284051529" POSITION="left" TEXT="intrinsic motivation"/>
+<node CREATED="1430284825828" ID="ID_1729375596" MODIFIED="1430914705370" POSITION="left" TEXT="visual attention">
+<node CREATED="1430284830889" ID="ID_1646158855" MODIFIED="1430284832390" TEXT="Nagai, Y., &amp; Rohlfing, K. J. (2009). Computational analysis of motionese toward scaffolding robot action learning. IEEE Transactions on Autonomous Mental Development, 1(1), 44&#x2013;54. http://doi.org/10.1109/TAMD.2009.2021090"/>
+</node>
+<node CREATED="1430285093757" ID="ID_1498346083" MODIFIED="1430914705732" POSITION="left" TEXT="Joint attention">
+<node CREATED="1430285099652" ID="ID_990392401" MODIFIED="1430285100557" TEXT="Rouanet, P., Oudeyer, P.-Y., &amp; Filliat, D. (2010). A study of three interfaces allowing non-expert users to teach new visual objects to a robot and their impact on learning efficiency. Human-Robot Interaction (HRI), 2010 5th ACM/IEEE International Conference on. http://doi.org/10.1109/HRI.2010.5453202"/>
+</node>
+<node CREATED="1430285337875" ID="ID_602874624" MODIFIED="1430914706115" POSITION="left" TEXT="local models">
+<node CREATED="1430285341035" ID="ID_1489056172" MODIFIED="1430285342293" TEXT="Atkeson, C. G., &amp; Schaal, S. (1995). Memory-based neural networks for robot learning. Neurocomputing. http://doi.org/10.1016/0925-2312(95)00033-6"/>
+</node>
+<node CREATED="1430285417787" ID="ID_1294972032" MODIFIED="1430914707203" POSITION="left" TEXT="locally weighted learning">
+<node CREATED="1430285428895" ID="ID_1694827104" MODIFIED="1430285429696" TEXT="Schaal, S., Atkeson, C. G., &amp; Vijayakumar, S. (2002). Scalable techniques from nonparametric statistics for real time robot learning. Applied Intelligence, 17(1), 49&#x2013;60. http://doi.org/10.1023/A:1015727715131"/>
+</node>
+<node CREATED="1430285509173" ID="ID_1889077708" MODIFIED="1430285517149" POSITION="right" TEXT="locally weighted projection regression">
+<node CREATED="1430285518078" ID="ID_932631356" MODIFIED="1430285519460" TEXT="Grollman, D. H., &amp; Jenkins, O. C. (2008). Sparse incremental learning for interactive robot control policy estimation. In 2008 IEEE International Conference on Robotics and Automation (pp. 3315&#x2013;3320). http://doi.org/10.1109/ROBOT.2008.4543716"/>
+</node>
+</node>
+</map>
